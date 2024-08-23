2aefba69345836e274482d6e719c19ad61defc2e
chaeyoon1101 <pgbenny0035@gmail.com>
2024-08-23 22:54:19 +0900

    FEAT: 그래프 바 생성

---
+++ PokemonGoLibrary/View/PokemonLibraryView/BarGraphView.swift
@@ -0,0 +1,28 @@
+//
+//  BarGraphView.swift
+//  PGoLib
+//
+//  Created by 임채윤 on 8/22/24.
+//
+
+import SwiftUI
+
+struct BarGraphView: View {
+    let value: CGFloat
+    let barColor: Color
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(barColor.opacity(0.3))
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+                
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(barColor)
+                    .frame(width: proxy.size.width * (value / 100), height: proxy.size.height - 4)
+            }
+        }
+        .frame(height: 8)
+    }
+}
